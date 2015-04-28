d1cd840c268d6db749e1af100ba7b0d54646474c
Vladimir Golubkov <vgol.mail@gmail.com>
2015-04-28 10:58:06 +0300

    .gitignore, MANIFEST.in and setup.py updated.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
+import os.path
+
+
+# ct - contfig template
+ct_src = 'modules/config-templates'
+ct_dest = 'lib/la/config-templates'
+
 
 setup(name = "la",
-      version = "0.1",
-      description = "Configure the services of Astra Linux SE.",
+      version = "0.1.0",
+      description = "Configure Astra Linux SE services.",
       author = "Vladimir Golubkov",
-      author_email = "support@rusbitech.ru",
+      author_email = "vgol.mail@gmail.com",
       url = "http://astra-linux.com",
       packages = ['la'],
       package_dir = {'la': 'modules'},
       scripts = ['la-run'],
-      long_description = """long description"""
+      data_files = [(ct_dest, [os.path.join(ct_src, 'ald.conf')])],
+      long_description = """Configure Astra Linux SE services."""
      )
